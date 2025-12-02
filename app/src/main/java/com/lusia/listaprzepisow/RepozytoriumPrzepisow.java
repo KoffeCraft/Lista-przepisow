102a16405e51d0596511a91a91f8e9410cd45233
Your Name <you@example.com>
2025-12-02 08:55:33 +0100

    przepisy repozytorium

--- app/src/main/java/com/lusia/listaprzepisow/RepozytoriumPrzepisow.java
+++ app/src/main/java/com/lusia/listaprzepisow/RepozytoriumPrzepisow.java
@@ -1,5 +1,17 @@
 package com.lusia.listaprzepisow;
 
-public class RepozytoriumPrzepisow {
+import java.util.ArrayList;
 
+public class RepozytoriumPrzepisow {
+    private static ArrayList<Przepis> przepisy;
+    private static void generujPrzepisy(){
+        przepisy = new ArrayList<>();
+        przepisy.add(new Przepis("Herbata zimowa", "Napoje", R.drawable.herbata, "Torebka herbaty, miód, cytryna", "Mniam mniam"));
+        przepisy.add(new Przepis("Barszcz czerwony", "Potrawy", R.drawable.barszcz, "Buraki, mąka, ziemniaki, zioła", "Zupa była za słona"));
+        przepisy.add(new Przepis("Serowe choinki", "Przekąski", R.drawable.serowehoinki, "Torebka herbaty, miód, cytryna", "Jingel bels"));
+    }
+    public ArrayList<Przepis> getPrzepisy(){
+        generujPrzepisy();
+        return przepisy;
+    }
 }
