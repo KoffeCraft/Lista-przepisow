282b6e8cbe21e62ccefbf046a048093379e1a68c
Your Name <you@example.com>
2025-12-02 08:25:41 +0100

    drugie activity i lista

--- app/src/main/java/com/lusia/listaprzepisow/MainActivity.java
+++ app/src/main/java/com/lusia/listaprzepisow/MainActivity.java
@@ -1,6 +1,12 @@
 package com.lusia.listaprzepisow;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +15,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ListView listViewKategorie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +26,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        listViewKategorie = findViewById(R.id.listViewKategorie);
+        listViewKategorie.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                view.setBackgroundColor(Color.GREEN);
+                Toast.makeText(MainActivity.this, "Wybrano kategorię"+i, Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(MainActivity.this, ListaPrzepisowActivity.class);
+                String kategoria = adapterView.getItemAtPosition(i).toString();
+                intent.putExtra("KATEGORIA", kategoria);
+                startActivity(intent);
+            }
+        });
+
     }
 }
